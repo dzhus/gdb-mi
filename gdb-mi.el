eea16eb02a55fdb43dd4eb9040e46ba7e724744b
Dmitry Dzhus <dima@sphinx.net.ru>
2009-08-08 17:31:19 +0400

    (gdb-wait-for-pending): Ultimate fix.

--- gdb-mi.el
+++ gdb-mi.el
@@ -252,15 +252,13 @@ Elements are either function names or pairs (buffer . function)")
   `(setq gdb-pending-triggers
          (delete ,item gdb-pending-triggers)))
 
-(defmacro gdb-wait-for-pending-timeout 0.5)
-
 (defmacro gdb-wait-for-pending (&rest body)
   "Wait until `gdb-pending-triggers' is empty and evaluate FORM.
 
 This function checks `gdb-pending-triggers' value every
 `gdb-wait-for-pending' seconds."
   (run-with-timer 
-   gdb-wait-for-pending-timeout nil
+   0.5 nil
    `(lambda ()
       (if (not gdb-pending-triggers)
           (progn ,@body)
